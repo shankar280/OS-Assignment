443804dee7f1e8e624c1eb3d0e9e99fb2ea5327d
shankar280 <49247924+shankar280@users.noreply.github.com>
2019-04-09 00:59:37 +0530

    Update final2.c

--- final2.c
+++ final2.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<unistd.h>
+#include<fcntl.h>
+#include<stdlib.h>
 int main()
 {
-  int t,responsetime,n;
-  printf("Process no.");
-  scanf("%d",&n);
+	int i,t[20],n;
+	int responsetime[20];
+	printf("process no: ");
+	scanf("%d",&n);
+	printf("Enter data\n");
