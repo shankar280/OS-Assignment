daa38c30bc6f39a86f40d406a1ad0bafe8158274
shankar280 <49247924+shankar280@users.noreply.github.com>
2019-04-10 00:26:53 +0530

    Update final2.c

--- final2.c
+++ final2.c
@@ -9,3 +9,16 @@ int main()
 	printf("Total number of process: ");
 	scanf("%d",&n);
 	printf("Provide data of the processes\n");
+	for(i=0;i<n;i++)
+	{
+	printf("Response time of P%d (in millisec.): ",i);
+	scanf("%d",&responsetime[i]);
+	if(responsetime[i]<1000)
+	{
+	t[i]=1;
+	}
+	else
+	{
+	t[i]=0;
+	}
+	}
